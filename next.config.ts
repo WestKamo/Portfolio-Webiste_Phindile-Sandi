2e12fc63777c794ea13238bb0bf728a17f69031d
WestKamo <phindilesandi07@gmail.com>
2026-08-11 19:07:39 +0200

    configure Next.js for github pages deploymemt via actions

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true, O
+  },
+  basePath: '/Portfolio-Webiste_Phindile-Sandi',
+  assetPrefix: '/Portfolio-Webiste_Phindile-Sandi',
 };
 
 export default nextConfig;
