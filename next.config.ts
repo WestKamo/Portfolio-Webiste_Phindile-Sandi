034f29cfb4d1947a06d12d172f71210607307318
WestKamo <phindilesandi07@gmail.com>
2026-08-12 16:16:37 +0200

    added EQF

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+cat << 'EOF' > next.config.ts
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
@@ -10,3 +11,4 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+EOF
